8922f16146d84a11511f662ac3b1e56236f0f8ac
Sunny Sahu <sunnysahu8210@gmail.com>
2024-04-03 00:04:14 +0530

    ReadMe Updated

--- src/app/store.js
+++ src/app/store.js
@@ -2,6 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import statusReducer from "../features/userSlice";
 
+//Creating the Store instance and Providing a Reducer instance
 export const store = configureStore({
   reducer: statusReducer,
 });
